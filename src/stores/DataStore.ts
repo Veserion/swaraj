0d1cf33d82b6653255abcdf8affc71de8b738f4d
Veserion <48389713+Veserion@users.noreply.github.com>
2020-06-02 01:48:17 +0300

    DataStore was done

--- src/stores/DataStore.ts
+++ src/stores/DataStore.ts
@@ -3,24 +3,20 @@ import { SubStore } from "./SubStore";
 import { database } from "../utils/firebase";
 import { action, observable } from "mobx";
 
-export interface IItem {
-  title: string;
-  model: string;
-  gen: string;
-  tags: string[];
-  weight: number;
+export interface IItem {}
+type TWearable = {
+  article: string;
+  quantityIssued: number;
+  materials: string;
+  designer: string;
   price: number;
-  oldPrice?: number;
-  stock: boolean;
   description: string;
-  attachments?: string[];
-  key?: string;
-  id?: string;
-}
+  picture: string;
+};
 
 export class DataStore extends SubStore {
-  @observable goods: { [key: string]: IItem } = {};
-  
+  @observable drops: Record<string, Record<string, TWearable>> = {};
+
   constructor(rootStore: RootStore, initState: any) {
     super(rootStore);
     this.syncGoods();
@@ -28,11 +24,29 @@ export class DataStore extends SubStore {
 
   @action syncGoods = async () => {
     database
-      .ref("goods")
+      .ref("/")
       .once("value")
       .then((snapshot) => {
-        const goods = snapshot.val();
-        this.goods = goods;
+        const drops = snapshot.val();
+        this.drops = drops;
       });
   };
+
+  addItem = async (item: IItem) =>
+    new Promise(async (resolve) => {
+      database.ref("/").push(item, (error) => resolve(error));
+      await this.syncGoods();
+    });
+
+  updateItem = async (id: string, item: IItem) =>
+    new Promise(async (resolve) => {
+      database.ref(`/${id}`).update(item, (error) => resolve(error));
+      await this.syncGoods();
+    });
+
+  removeItem = async (id: string) =>
+    new Promise(async (resolve) => {
+      database.ref(`/${id}`).remove((error) => resolve(error));
+      await this.syncGoods();
+    });
 }
