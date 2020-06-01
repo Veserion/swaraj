bf120063daa0519ff01ac31506e7d08003ed48a3
Veserion <48389713+Veserion@users.noreply.github.com>
2020-06-02 02:21:40 +0300

    SwitchScreen

---
+++ src/components/SwitchScreen/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from '@emotion/styled'
+
+
+export default class SwitchScreen extends React.Component {
+    handleSwitch = () => {
+
+    }
+    render() {
+        return <Root>
+            <Drops></Drops>
+            <PushDrop></PushDrop>
+        </Root>
+    }
+}
+
+const Root = styled.div`
+position: absolute;
+display: flex;
+transition: all 0.3s;
+.isOpen{
+    background: #1890FF;
+}
+`
+
+const Drops = styled.div`
+padding: 10px 15px;
+`
+const PushDrop = styled.div`
+padding: 10px 15px;
+`
