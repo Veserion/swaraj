74e5fc35c29d15003490e1dcec1d6d689f0990ce
Veserion <48389713+Veserion@users.noreply.github.com>
2020-06-09 00:28:49 +0300

    Updates

---
+++ precache-manifest.01d0e02e0a4920b8d024f233b5ba2186.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7370861f6735149b2fa2375c047c4ac9",
+    "url": "/swaraj/index.html"
+  },
+  {
+    "revision": "9987ecfb317c68cbfeff",
+    "url": "/swaraj/static/css/2.42ec2a3e.chunk.css"
+  },
+  {
+    "revision": "9987ecfb317c68cbfeff",
+    "url": "/swaraj/static/js/2.d6e8a3d5.chunk.js"
+  },
+  {
+    "revision": "49fe2bc6ca6e77e8e48c05ef1eac3fbf",
+    "url": "/swaraj/static/js/2.d6e8a3d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5536fda0cfacc0204764",
+    "url": "/swaraj/static/js/main.3d3642df.chunk.js"
+  },
+  {
+    "revision": "cc1adb78c1ae81ba061e",
+    "url": "/swaraj/static/js/runtime-main.53862a8e.js"
+  }
+]);
